1aecc7296316db03a96f80716a4b0ec81151bc85
Dixant Mittal <dixant.mittal@gmail.com>
2017-12-13 15:56:35 +0800

    adds skeleton client > need to integrate learned model

---
+++ learned_client.py
@@ -0,0 +1,155 @@
+from __future__ import print_function
+
+import argparse
+import logging
+import random
+import sys
+import time
+
+from carla.client import make_carla_client
+from carla.sensor import Camera
+from carla.settings import CarlaSettings
+from carla.tcp import TCPConnectionError
+
+
+def getControls(image):
+    return None, None
+
+
+def run_carla_client(host, port, autopilot_on, save_images_to_disk, image_filename_format, settings_filepath):
+    number_of_episodes = 5
+    frames_per_episode = 300
+
+    with make_carla_client(host, port) as client:
+        print('CarlaClient connected')
+
+        for episode in range(0, number_of_episodes):
+            # Start a new episode.
+
+            # Create a CarlaSettings object. This object is a wrapper around
+            # the CarlaSettings.ini file. Here we set the configuration we
+            # want for the new episode.
+            settings = CarlaSettings()
+            settings.set(
+                SynchronousMode=True,
+                SendNonPlayerAgentsInfo=True,
+                NumberOfVehicles=50,
+                NumberOfPedestrians=100,
+                WeatherId=random.choice([1, 3, 7, 8, 14]))
+            settings.randomize_seeds()
+
+            # Now we want to add a couple of cameras to the player vehicle.
+            # We will collect the images produced by these cameras every
+            # frame.
+
+            # The default camera captures RGB images of the scene.
+            settings.add_sensor(Camera('CameraRGB', image_size=(1280, 720), position=(8, 0, 140), rotation=(-12, 0, 0)))
+
+            scene = client.load_settings(settings)
+
+            # Choose one player start at random.
+            player_start = 0
+
+            # Notify the server that we want to start the episode at the
+            # player_start index. This function blocks until the server is ready
+            # to start the episode.
+            print('Starting new episode...')
+            client.start_episode(player_start)
+
+            # Iterate every frame in the episode.
+            for frame in range(frames_per_episode):
+
+                # Read the data produced by the server this frame.
+                measurements, sensor_data = client.read_data()
+
+                # Save the images to disk if requested.
+                if save_images_to_disk:
+                    for name, image in sensor_data.items():
+                        image.save_to_disk(image_filename_format.format(episode, name, frame))
+
+                # Now we have to send the instructions to control the vehicle.
+                # If we are in synchronous mode the server will pause the
+                # simulation until we send this control.
+
+                # get control instructions from model
+                with image as sensor_data['CameraRGB']:
+                    steer, throttle = getControls(image)
+
+                print(steer, throttle)
+
+                # send control to simulator
+                client.send_control(
+                    steer=steer,
+                    throttle=throttle,
+                    brake=0.0,
+                    hand_brake=False,
+                    reverse=False)
+
+
+def main():
+    argparser = argparse.ArgumentParser(description=__doc__)
+    argparser.add_argument(
+        '-v', '--verbose',
+        action='store_true',
+        dest='debug',
+        help='print debug information')
+    argparser.add_argument(
+        '--host',
+        metavar='H',
+        default='localhost',
+        help='IP of the host server (default: localhost)')
+    argparser.add_argument(
+        '-p', '--port',
+        metavar='P',
+        default=2000,
+        type=int,
+        help='TCP port to listen to (default: 2000)')
+    argparser.add_argument(
+        '-a', '--autopilot',
+        action='store_true',
+        help='enable autopilot')
+    argparser.add_argument(
+        '-i', '--images-to-disk',
+        action='store_false',
+        help='save images to disk')
+    argparser.add_argument(
+        '-c', '--carla-settings',
+        metavar='PATH',
+        default=None,
+        help='Path to a "CarlaSettings.ini" file')
+
+    args = argparser.parse_args()
+
+    log_level = logging.DEBUG if args.debug else logging.INFO
+    logging.basicConfig(format='%(levelname)s: %(message)s', level=log_level)
+
+    logging.info('listening to server %s:%s', args.host, args.port)
+
+    while True:
+        try:
+
+            run_carla_client(
+                host=args.host,
+                port=args.port,
+                autopilot_on=args.autopilot,
+                save_images_to_disk=args.images_to_disk,
+                image_filename_format='_images/episode_{:0>3d}/{:s}/image_{:0>5d}.png',
+                settings_filepath=args.carla_settings)
+
+            print('Done.')
+            return
+
+        except TCPConnectionError as error:
+            logging.error(error)
+            time.sleep(1)
+        except Exception as exception:
+            logging.exception(exception)
+            sys.exit(1)
+
+
+if __name__ == '__main__':
+
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('\nCancelled by user. Bye!')
